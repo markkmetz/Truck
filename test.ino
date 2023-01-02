02019fd2a82fe7e83684db1c92c7692df5f56141
markkmetz <90885460+markkmetz@users.noreply.github.com>
2023-01-02 16:09:35 -0600

    added charts

--- test.ino
+++ test.ino
@@ -1,3 +1,21 @@
+//solenoid/clutch apply chart-----
+// PRN1 1/0
+// 2 0/0
+// 3 0/1
+// 4 1/1
+
+//Curves--------------------------
+//1st gear UP = 0.389x +5.11
+//
+//2nd gear DOWN = 0.333x +3.67
+//2nd gear UP = 0.778x +10.2
+//
+//3rd gear DOWN = 0.722x +8.78
+//3rd gear UP = 1.17x +14.3
+//
+//4th gear DOWN = 1.06x +13.4
+//
+//Line Pressure = 0.389x +56.1
 
 void setup() {
   pinMode(2, OUTPUT);
