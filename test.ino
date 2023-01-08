0911dd6bed8883bd2af823c752c107a74764704e
markkmetz <90885460+markkmetz@users.noreply.github.com>
2023-01-07 23:10:33 -0600

    mph fixed, but having issues with data

--- test.ino
+++ test.ino
@@ -1,44 +1,48 @@
-//solenoid/clutch apply chart-----
-// PRN1 1/0
-// 2 0/0
-// 3 0/1
-// 4 1/1
+// solenoid/clutch apply chart-----
+//  PRN1 1/0
+//  2 0/0
+//  3 0/1
+//  4 1/1
 
-//Shift Curves--------------------------
-//1st gear UP = 0.389x +5.11
+// Shift Curves--------------------------
+// 1st gear UP = 0.389x +5.11
 //
-//2nd gear DOWN = 0.333x +3.67
-//2nd gear UP = 0.778x +10.2
+// 2nd gear DOWN = 0.333x +3.67
+// 2nd gear UP = 0.778x +10.2
 //
-//3rd gear DOWN = 0.722x +8.78
-//3rd gear UP = 1.17x +14.3
+// 3rd gear DOWN = 0.722x +8.78
+// 3rd gear UP = 1.17x +14.3
 //
-//4th gear DOWN = 1.06x +13.4
+// 4th gear DOWN = 1.06x +13.4
 
-//TODO
-//tcc unlock = 20%
-//tcc lockup time 3 seconds 30%-70%
-//tcc unlock on throttle off?
-//unlock tcc as you shift?
+// TODO
+// tcc unlock = 20%
+// tcc lockup time 3 seconds 30%-70%
+// tcc unlock on throttle off?
+// unlock tcc as you shift?
 
-//EPC
-//Line Pressure = 0.389x +56.1
-//290hz?
+// EPC
+// Line Pressure = 0.389x +56.1
+// 290hz?
 
-//INPUTS
+// INPUTS
 const byte Load_Pin = A4;
 const byte ISS_Pin = 3;
 const byte OSS_Pin = 2;
 const byte LinePressure_Pin = A1;
 const byte EPCPressure_Pin = A0;
 
-//OUTPUTS
-int TCC_Pin = 9; //make sure this is an analog pin
-int SolA_Pin = 10; 
+// OUTPUTS
+int TCC_Pin = 9; // make sure this is an analog pin
+int SolA_Pin = 10;
 int SolB_Pin = 11;
-int EPC_Pin = 6; //make sure this is an analog pin
+int EPC_Pin = 6; // make sure this is an analog pin
 
-//Constants
+// Constants
+const int OSS_Holes = 12;
+const double GearRatio = 3.55;
+const double TireSize = 33;
+
 const int TCC_Max = 0.75 * 255;
 const int TCC_Min = 0.3 * 255;
 const int EPC_Max = 0.75 * 255;
@@ -47,27 +51,28 @@ const int Load_Smoothing = 1;
 const int ISS_Smoothing = 5;
 const int OSS_Smoothing = 5;
 
-//Variables
-int speeds = 0;
-
-int Load [Load_Smoothing];
+// Variables
+int Load[Load_Smoothing];
 int LoadHigh = 0;
 
-int ISS [ISS_Smoothing];
+int ISS[ISS_Smoothing];
 int ISSHigh = 0;
 
-int OSS [OSS_Smoothing];
+int OSS[OSS_Smoothing];
+int OSS_Speeds[OSS_Smoothing];
+int OSS_Speed_Count = 0;
+int OSS_Measure_Count = 0;
 int OSSHigh = 0;
 
 unsigned long previousMillis;
 unsigned long currentMillis;
-int count = 0;
 
 int CurrentGear = 0;
 int DesiredGear = 0;
 
-void setup() {
-  #pragma region PinStates
+void setup()
+{
+#pragma region PinStates
   pinMode(TCC_Pin, OUTPUT);
   pinMode(SolA_Pin, OUTPUT);
   pinMode(SolB_Pin, OUTPUT);
@@ -79,146 +84,158 @@ void setup() {
   pinMode(LinePressure_Pin, INPUT);
   pinMode(EPCPressure_Pin, INPUT);
 
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
 
-  //RE-ENABLE THIS WHEN YOU ARE READY TO DRIVE!!!!-----------
-  //prepare outputs for vehicle startup
-  //analogWrite(EPC_Pin, EPC_Max);
-  //analogWrite(TCC_Pin, TCC_Max);
-  //digitalWrite(SolA_Pin, HIGH);
-  //digitalWrite(SolA_Pin, LOW);
-  #pragma endregion PinStates
-  
+// RE-ENABLE THIS WHEN YOU ARE READY TO DRIVE!!!!-----------
+// prepare outputs for vehicle startup
+// analogWrite(EPC_Pin, EPC_Max);
+// analogWrite(TCC_Pin, TCC_Max);
+// digitalWrite(SolA_Pin, HIGH);
+// digitalWrite(SolA_Pin, LOW);
+#pragma endregion PinStates
+
   previousMillis = millis();
 
   Serial.begin(9600);
 
-  //analogtest();
-  //digitaltest();
+  // analogtest();
+  // digitaltest();
 }
 
-void measurespeed(){
+void measurespeed()
+{
   currentMillis = millis();
-  OSS[count] = digitalRead(OSS_Pin);
+  OSS[OSS_Measure_Count] = digitalRead(OSS_Pin);
   bool haschanged = false;
   unsigned long timebetween;
   double hz;
-  //reset array
-  if(count < OSS_Smoothing)
-    count++;
-  else{
-    count = 0;
+  // reset array
+  if (OSS_Measure_Count < OSS_Smoothing)
+    OSS_Measure_Count++;
+  else
+  {
+    OSS_Measure_Count = 0;
   }
 
   int osssum = 0;
-  for(int i=0;i<OSS_Smoothing;i++){
+  for (int i = 0; i < OSS_Smoothing; i++)
+  {
     osssum += OSS[i];
   }
-  //Serial.println(count);
-  if (osssum > OSS_Smoothing-2 and OSSHigh == 0){
+  // Serial.println(count);
+  if (osssum > OSS_Smoothing - 2 and OSSHigh == 0)
+  {
     OSSHigh = 1;
-    digitalWrite(13,HIGH);
+    digitalWrite(13, HIGH);
     timebetween = currentMillis - previousMillis;
-    hz = (1.00/timebetween)*1000;
-    Serial.println(hz);
+    hz = (1.00 / timebetween) * 1000;
+    double s =  6.283185307  * (TireSize / 2.00) * (((hz / OSS_Holes) * GearRatio ) / 60);
+    Serial.println(s);
 
+    // if (OSS_Measure_Count < OSS_Smoothing)
+    //   OSS_Measure_Count++;
+    // else
+    // {
+    //   OSS_Measure_Count = 0;
+    // }
   }
 
-  if (osssum < OSS_Smoothing -2 and OSSHigh == 1){
+  if (osssum < OSS_Smoothing - 2 and OSSHigh == 1)
+  {
     OSSHigh = 0;
-    digitalWrite(13,LOW);
+    digitalWrite(13, LOW);
     previousMillis = currentMillis;
   }
-
-
-
 }
 
-void digitaltest(){
+void digitaltest()
+{
 
-  digitalWrite(EPC_Pin,HIGH);
+  digitalWrite(EPC_Pin, HIGH);
   delay(1000);
-  digitalWrite(EPC_Pin,LOW);
-
-  delay(1000);
-  digitalWrite(SolA_Pin,HIGH);
-  delay(1000);
-  digitalWrite(SolA_Pin,LOW);
+  digitalWrite(EPC_Pin, LOW);
 
   delay(1000);
-  digitalWrite(SolB_Pin,HIGH);
+  digitalWrite(SolA_Pin, HIGH);
   delay(1000);
-  digitalWrite(SolB_Pin,LOW);
+  digitalWrite(SolA_Pin, LOW);
 
   delay(1000);
-  digitalWrite(TCC_Pin,HIGH);
+  digitalWrite(SolB_Pin, HIGH);
   delay(1000);
-  digitalWrite(TCC_Pin,LOW);
+  digitalWrite(SolB_Pin, LOW);
+
+  delay(1000);
+  digitalWrite(TCC_Pin, HIGH);
+  delay(1000);
+  digitalWrite(TCC_Pin, LOW);
 }
 
-void analogtest(){
+void analogtest()
+{
 
   delay(1000);
-  analogWrite(TCC_Pin,50);
+  analogWrite(TCC_Pin, 50);
   delay(1000);
-  analogWrite(TCC_Pin,100);
+  analogWrite(TCC_Pin, 100);
   delay(1000);
-  analogWrite(TCC_Pin,150);
+  analogWrite(TCC_Pin, 150);
   delay(1000);
-  analogWrite(TCC_Pin,200);
+  analogWrite(TCC_Pin, 200);
   delay(1000);
-  analogWrite(TCC_Pin,250);
+  analogWrite(TCC_Pin, 250);
   delay(1000);
-  analogWrite(TCC_Pin,0);
+  analogWrite(TCC_Pin, 0);
 
   delay(1000);
-  analogWrite(EPC_Pin,50);
+  analogWrite(EPC_Pin, 50);
   delay(1000);
-  analogWrite(EPC_Pin,100);
+  analogWrite(EPC_Pin, 100);
   delay(1000);
-  analogWrite(EPC_Pin,150);
+  analogWrite(EPC_Pin, 150);
   delay(1000);
-  analogWrite(EPC_Pin,200);
+  analogWrite(EPC_Pin, 200);
   delay(1000);
-  analogWrite(EPC_Pin,250);
+  analogWrite(EPC_Pin, 250);
   delay(1000);
-  analogWrite(EPC_Pin,0);
+  analogWrite(EPC_Pin, 0);
 
   delay(1000);
-  analogWrite(SolA_Pin,50);
+  analogWrite(SolA_Pin, 50);
   delay(1000);
-  analogWrite(SolA_Pin,100);
+  analogWrite(SolA_Pin, 100);
   delay(1000);
-  analogWrite(SolA_Pin,150);
+  analogWrite(SolA_Pin, 150);
   delay(1000);
-  analogWrite(SolA_Pin,200);
+  analogWrite(SolA_Pin, 200);
   delay(1000);
-  analogWrite(SolA_Pin,250);
+  analogWrite(SolA_Pin, 250);
   delay(1000);
-  analogWrite(SolA_Pin,0);
+  analogWrite(SolA_Pin, 0);
 
   delay(1000);
-  analogWrite(SolB_Pin,50);
+  analogWrite(SolB_Pin, 50);
   delay(1000);
-  analogWrite(SolB_Pin,100);
+  analogWrite(SolB_Pin, 100);
   delay(1000);
-  analogWrite(SolB_Pin,150);
+  analogWrite(SolB_Pin, 150);
   delay(1000);
-  analogWrite(SolB_Pin,200);
+  analogWrite(SolB_Pin, 200);
   delay(1000);
-  analogWrite(SolB_Pin,250);
+  analogWrite(SolB_Pin, 250);
   delay(1000);
-  analogWrite(SolB_Pin,0);
+  analogWrite(SolB_Pin, 0);
 }
 
-void osstest(){
-  
+void osstest()
+{
 }
 
-void loop() {
+void loop()
+{
   measurespeed();
-  //currentMillis = millis();
-  //test();
-  //Serial.println(analogRead(EPCPressure_Pin));
-  //Serial.println(digitalRead(OSS_Pin));
+  // currentMillis = millis();
+  // test();
+  // Serial.println(analogRead(EPCPressure_Pin));
+  // Serial.println(digitalRead(OSS_Pin));
 }
