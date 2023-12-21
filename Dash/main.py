26e45a43548021be5abb7a7f094b9183fb2cef94
markkmetz <90885460+markkmetz@users.noreply.github.com>
2023-12-21 12:35:08 -0600

    bunch of changes. added more meters/progress bars started can communication. haven't tested..

--- Dash/main.py
+++ Dash/main.py
@@ -1,31 +1,77 @@
 #scp C:\Users\markl\OneDrive\Documents\GitHub\Truck\Dash\main.py mark@192.168.1.253:/home/mark
 # sudo apt install python3-ttkbootstrap
+#start can:
+#sudo ip link set can0 up type can bitrate 500000
+
 
 from cgitb import text
-from turtle import bgcolor, width
+from socket import timeout
+from turtle import bgcolor, color, width
+from numpy import angle
+from pyscreeze import center
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import Frame, X, StringVar
 import time
 from PIL import Image,ImageTk
 import sys
+import platform
+import can
+
 
 app = ttk.Window()
-#app.minsize(1920,515)
-
 style = ttk.Style(theme='darkly')
-
 #app.attributes('-fullscreen', True)
 frame = ttk.Frame(app, width=1920, height=515)
-#frame.pack_propagate(0)  # Don't shrink or expand the frame
 frame.pack()
+os_type = platform.system()
+timeout_counter = 0
+large_font = ttk.font.Font(family='Helvetica', size=20, weight='bold')
+xl_font = ttk.font.Font(family='Helvetica', size=30, weight='bold')
+med_font = ttk.font.Font(size=15)
+small_font = ttk.font.Font(size=8)
 
-def toggle_led():
-    if led_indicator['style'] == 'Green.TLabel':
-        led_indicator['style'] = 'Red.TLabel'
-    else:
-        led_indicator['style'] = 'Green.TLabel'
+values = {
+'rpmValue' : 0,
+'tpsValue' : 0,
+'tempValue' : 0,
+'speedValue' : 0,
+'gearValue' : 0,
+'tccValue' : 0
+}
 
+
+def receive_can_messages(channel):
+    global timeout_counter
+    global values
+    bus = can.interface.Bus(channel=channel, bustype='socketcan')
+    while True:
+        canMsg = bus.recv(.5)
+        if canMsg is not None:  
+            if canMsg.arbitration_id == "5f0":
+                values['rpmValue'] = canMsg.data[7] | (canMsg.data[6] << 8)
+            elif canMsg.arbitration_id == "5f3":
+                values['tps'] = canMsg.data[1] | (canMsg.data[0] << 8)
+            
+        else:
+            print("Timeout")
+
+
+def update():
+
+
+
+    if os_type == "Linux":
+        receive_can_messages('can0')
+
+    meters['RPM'].configure(amountused=values['rpmValue'])
+    #this is a progress bar not a meter. so this might vary
+    #meters['loadValue'].configure(amountused=values['tpsValue'])
+    
+    app.after(100, update)  # 100 milliseconds = 0.1 seconds
+
+
+#region meters
 
 meters = {}
 #-----------------------------------------------------
@@ -154,13 +200,28 @@ meter = ttk.Meter(
     arcrange=120,
     arcoffset=150,
     amounttotal=250,
-    subtext='EPC',
+    #subtext='EPC',
     interactive=False,
-    textright='Psi',
-    meterthickness=30
+    #textleft='Psi',
+    meterthickness=30,
+    showtext=False
 )
 meter.place(x=1750,y=0)
 meters['EPC'] = meter
+
+label = ttk.Label(frame, text="120", font=large_font,foreground="#375a7f")
+label.place(x=1810, y=90)
+label = ttk.Label(frame, text="Psi")
+label.place(x=1860, y=105)
+
+label = ttk.Label(frame, text="EPC", font=med_font)
+label.place(x=1830, y=140)
+
+progressbar = ttk.Progressbar(frame, value=75, orient='vertical',length=170)
+progressbar.place(x=1890,y=8,width=20)
+
+label = ttk.Label(frame, text="PWM", font=small_font)
+label.place(x=1885, y=180)
 
 
 #-----------------------------------------------------
@@ -174,37 +235,50 @@ meter = ttk.Meter(
     arcrange=120,
     arcoffset=150,
     amounttotal=4,
-    subtext='Gear',
+    
     interactive=False,
-    meterthickness=30
+    meterthickness=30,
+    showtext=False
 )
 meter.place(x=1750,y=210)
 meters['Gear'] = meter
 
+label = ttk.Label(frame, text="1", font=xl_font,foreground="#375a7f")
+label.place(x=1840, y=210 + 90)
+# label = ttk.Label(frame, text="")
+# label.place(x=1860, y=210+ 105)
+label = ttk.Label(frame, text="Gear", font=med_font)
+label.place(x=1830, y=210 + 140)
+
+progressbar = ttk.Progressbar(frame, value=75, orient='vertical',length=170)
+progressbar.place(x=1890,y=210 + 8,width=20)
+label = ttk.Label(frame, text="Load", font=small_font)
+label.place(x=1885, y=210+ 180)
+
+
 #-----------------------------------------------------
 #   TCC indicator
 
-style.configure('Green.TLabel', background='green', foreground='green', borderwidth=1, relief="solid",width=3)
-style.configure('Red.TLabel', background='red', foreground='red', borderwidth=1, relief="solid")
+tcc = ttk.Radiobutton(style="info",text="TCC")
+tcc.place(x=1830,y=265)
 
-led_indicator = ttk.Label(app, style='Red.TLabel',text="sdf",width=3)
-led_indicator.place(x=1840,y=300)
-toggle_led()
+checkengine = ttk.Radiobutton(style="warning",text="Check Engine")
+checkengine.place(x=500,y=10)
 
 
 #-----------------------------------------------------
-
-def update_meters(new_dict):
-    for key, value in new_dict.items():
-        if key in meters:
-            meters[key].configure(amountused=value)
-    app.after(100, update_meters, new_dict)  # 100 milliseconds = 0.1 seconds
-
-def stop_script():
-    sys.exit()
-
 text_box = ttk.Text(frame, height=2)
 text_box.place(x=0,y=460,relwidth=1)
+text_box.insert('1.0',"Initialized.. Connecting to CAN...")
+
+odo = ttk.Text(frame, height=1)
+odo.place(x=650,y=390,width=100)
+odo.tag_configure('right', justify='right')
+odo.insert('0.0',"347,000")
 
 #-----------------------------------------------------
+
+#endregion
+
+update()
 app.mainloop()
